b3ff860adcead76db7a687ff2305df116d34f718
kappino <crescenzo.esposito@outlook.it>
2023-03-29 13:04:41 +0200

    Testato funzionamento classe mazzo

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,11 @@ public class Main {
         public static void main(String[] args){
                 Carta cartaa = new Carta(10,"coppe");
                 System.out.println(cartaa.getSeme());
+                Mazzo mazzo = new Mazzo();
+                mazzo.stampa();
+                mazzo.mescola();
+                System.out.println("Mazzo Mescolato:");
+                mazzo.stampa();
         }
         
 }
