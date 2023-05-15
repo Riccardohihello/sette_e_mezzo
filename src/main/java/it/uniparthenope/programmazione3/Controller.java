08d8e0e93ab85dfe8ffe17e366c9f0c2d0f38b0e
RiccardoHihello <riccardoaiello73@gmail.com>
2023-05-15 08:56:12 +0200

    Modificato il menu

--- src/main/java/it/uniparthenope/programmazione3/Controller.java
+++ src/main/java/it/uniparthenope/programmazione3/Controller.java
@@ -3,9 +3,14 @@ package it.uniparthenope.programmazione3;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class Controller {
     @FXML
     private Label welcomeText;
+
+    @FXML
+    private void exitButton(){
+        System.exit(0);
+    }
 
     @FXML
     protected void onHelloButtonClick() {
--- src/main/java/it/uniparthenope/programmazione3/View.java
+++ src/main/java/it/uniparthenope/programmazione3/View.java
@@ -8,10 +8,10 @@ import javafx.stage.Stage;
 import java.io.IOException;
 import java.util.Objects;
 
-public class HelloApplication extends Application {
+public class View extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(View.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1024, 1024);
         scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("styles.css")).toExternalForm());
         stage.setTitle("sette e mezzo");
