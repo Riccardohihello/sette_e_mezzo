dbc3a41feadb8078e8d8463144c6f93176b7de83
RiccardoHihello <riccardoaiello73@gmail.com>
2023-05-15 11:24:42 +0200

    Aggiunto metodo per cambiare scena nella classe View Creato il file fxml per la gameView

--- src/main/java/it/uniparthenope/programmazione3/Controller.java
+++ src/main/java/it/uniparthenope/programmazione3/Controller.java
@@ -1,19 +1,21 @@
 package it.uniparthenope.programmazione3;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
 
 public class Controller {
-    @FXML
-    private Label welcomeText;
-
     @FXML
     private void exitButton(){
         System.exit(0);
     }
 
+    // Metodo per cambiare scena
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void gameSceneButton(ActionEvent event) throws Exception {
+        View.cambiaScena("Game.fxml", (Stage) ((Node) event.getSource()).getScene().getWindow());
     }
 }
